fa494e7dd875558bd36c89a5f363131270227a63
pi12138 <zhou.youpeng@foxmail.com>
2025-04-02 18:12:28 +0800

    feat: docker nvim for go

--- nvim/Dockerfile.go
+++ nvim/Dockerfile.go
@@ -8,7 +8,7 @@ RUN cp /etc/apt/sources.list.d/debian.sources /etc/apt/sources.list.d/debian.sou
     && echo "Components: main contrib non-free non-free-firmware" >> /etc/apt/sources.list.d/debian.sources \ 
     && echo "Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg" >> /etc/apt/sources.list.d/debian.sources 
 RUN apt-get update \
-    && apt-get install -y git python3-pip luarocks ripgrep fd-find python3-neovim curl wget sudo \ 
+    && apt-get install -y git luarocks ripgrep fd-find curl wget sudo \ 
     && apt-get clean \
     && useradd -m -s /bin/bash ${USER} \
     && usermod -aG sudo ${USER} \
@@ -20,7 +20,9 @@ ENV HOME=/home/${USER}
 WORKDIR $HOME
 RUN mkdir -p $HOME/bin \
     && mkdir -p $HOME/apps \ 
-    && mkdir -p $HOME/.config \ 
+    && mkdir -p $HOME/.config \
+    && mkdir -p $HOME/.local/share/nvim/lazy \
+    && git clone --filter=blob:none https://github.com/folke/lazy.nvim.git --branch=stable $HOME/.local/share/nvim/lazy/lazy.nvim \ 
     && wget https://github.com/tree-sitter/tree-sitter/releases/download/v0.25.3/tree-sitter-linux-x64.gz \
     && gzip -d tree-sitter-linux-x64.gz \
     && chmod +x tree-sitter-linux-x64 \
@@ -33,11 +35,12 @@ RUN mkdir -p $HOME/bin \
     && ln -sf $HOME/apps/nvim-linux-x86_64/bin/nvim $HOME/bin/nvim \
     && rm nvim-linux-x86_64.tar.gz \
     && go env -w GOPROXY=https://goproxy.io,direct \
-    && go env -w GO111MODULE='auto' \ 
-    && go install golang.org/x/tools/gopls@latest \
-    && go clean -cache -modcache
+    && go env -w GO111MODULE='auto'
 
 COPY --chown=${USER}:${USER} . $HOME/.config/nvim
+RUN cd $HOME/.config/nvim \ 
+    && sed -i 's/require("plugins-config.lsp.lua")/-- require("plugins-config.lsp.lua")/' init.lua \ 
+    && sed -i 's/require("plugins-config.lsp.python")/-- require("plugins-config.lsp.python")/' init.lua
 ENV PATH="$HOME/bin:$PATH"
 ENV LANG=C.utf8 LC_ALL=C.utf8 LC_CTYPE=C.utf8 TZ=Asia/Shanghai
 CMD ["sleep", "infinity"]
