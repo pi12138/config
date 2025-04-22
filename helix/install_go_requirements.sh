67f233a36565b1e54289389b0c29dc82e21ec432
zyp <zyp>
2025-04-22 10:43:24 +0800

    feat: update golangci-lint to v2

--- helix/install_go_requirements.sh
+++ helix/install_go_requirements.sh
@@ -4,4 +4,5 @@ go install golang.org/x/tools/gopls@latest                            # LSP
 go install github.com/go-delve/delve/cmd/dlv@latest                   # Debugger
 go install golang.org/x/tools/cmd/goimports@latest                    # Formatter
 go install github.com/nametake/golangci-lint-langserver@latest        # Linter
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest # Linter cli
+# go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest # Linter cli
+go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest
